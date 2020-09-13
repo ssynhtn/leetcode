44cef6b6f04ac3abfb0b0f628cf10b22b710bdd1
ssynhtn <ssynhtn@gmail.com>
2020-09-13 23:10:31 +0800

    save

---
+++ src/com/ssynhtn/medium/InsertInterval.kt
@@ -0,0 +1,10 @@
+package com.ssynhtn.medium
+
+import java.util.*
+
+class InsertInterval {
+    fun insert(intervals: Array<IntArray>, newInterval: IntArray): Array<IntArray> {
+        val result = mutableListOf<IntArray>()
+
+    }
+}
