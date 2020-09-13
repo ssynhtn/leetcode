0822afadf70b9ee22d232bde0d3f654a8d9c55a9
ssynhtn <ssynhtn@gmail.com>
2020-09-13 21:59:14 +0800

    max sub array divider and conquer

--- src/com/ssynhtn/medium/MaximumSubarray.kt
+++ src/com/ssynhtn/medium/MaximumSubarray.kt
@@ -1,6 +1,36 @@
 package com.ssynhtn.medium
 
 class MaximumSubarray {
+    fun maxSubArrayDivConquer(nums: IntArray): Int {
+        return maxSubArrayDivConquer(nums, 0, nums.size)[0] // max, left max, right max, total, each requiring at least length one
+    }
+
+    // end > start
+    fun maxSubArrayDivConquer(nums: IntArray, start: Int, end: Int): IntArray {
+        if (end - start == 1) {
+            val ans = nums[start]
+            return intArrayOf(ans, ans, ans, ans)
+        }
+
+        val mid = (start + end) / 2
+        val max1 = maxSubArrayDivConquer(nums, start, mid)
+        val max2 = maxSubArrayDivConquer(nums, mid, end)
+
+//        val total = max1[3] + max2[3]
+//        val leftMax = Math.max(max1[1], max1[3] + max2[1])
+//        val rightMax = Math.max(max2[2], max2[3] + max1[2])
+//        val max = Math.max(max1[0], Math.max(max2[0], max1[2] + max2[1]))
+
+        max1[0] = Math.max(max1[0], Math.max(max2[0], max1[2] + max2[1]))
+        max1[1] = Math.max(max1[1], max1[3] + max2[1])
+        max1[2] = Math.max(max2[2], max2[3] + max1[2])
+        max1[3] = max1[3] + max2[3]
+
+        return max1
+
+    }
+
+
     fun maxSubArray(nums: IntArray): Int {
         var max = Int.MIN_VALUE
 
@@ -25,8 +55,9 @@ class MaximumSubarray {
 }
 
 fun main(args: Array<String>) {
-    println(MaximumSubarray().maxSubArray(intArrayOf(1)))
-    println(MaximumSubarray().maxSubArray(intArrayOf(0)))
-    println(MaximumSubarray().maxSubArray(intArrayOf(-1)))
-    println(MaximumSubarray().maxSubArray(intArrayOf(-2147483647)))
+    println(MaximumSubarray().maxSubArrayDivConquer(intArrayOf(-2,1,-3,4,-1,2,1,-5,4)))
+    println(MaximumSubarray().maxSubArrayDivConquer(intArrayOf(1)))
+    println(MaximumSubarray().maxSubArrayDivConquer(intArrayOf(0)))
+    println(MaximumSubarray().maxSubArrayDivConquer(intArrayOf(-1)))
+    println(MaximumSubarray().maxSubArrayDivConquer(intArrayOf(-2147483647)))
 }
