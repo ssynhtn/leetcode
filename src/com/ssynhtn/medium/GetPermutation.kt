a4147e9053761d615ba7c5ce51efe9b8f0806971
huangtongnao <huangtongnao@gmail.com>
2020-09-14 14:47:49 +0800

    get permutatoin

---
+++ src/com/ssynhtn/medium/GetPermutation.kt
@@ -0,0 +1,37 @@
+package com.ssynhtn.medium
+
+class GetPermutation {
+    fun getPermutation(n: Int, k: Int): String {
+        val chs = CharArray(n)
+        for (i in 1 .. n) {
+            chs[i - 1] = '0'.plus(i)
+        }
+        var index = k - 1
+        val facts = IntArray(n - 1)
+        var f = 1
+        for (i in 1 .. n-1) {
+            f = f * i
+            facts[n - i - 1] = f
+        }
+
+        for (i in 0 until n - 1) {
+            val multiple = index / facts[i]
+            index = index % facts[i]
+
+            shiftRight(chs, i, multiple)
+
+        }
+        return String(chs)
+    }
+
+    private fun shiftRight(chs: CharArray, i: Int, len: Int) {
+        if (len == 0) return
+        val temp = chs[i + len]
+        System.arraycopy(chs, i, chs, i + 1, len)
+        chs[i] = temp
+    }
+}
+
+fun main() {
+    println(GetPermutation().getPermutation(3, 3))
+}
