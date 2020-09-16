33e2183909fb012c826afe4f67e42cf801ab7869
huangtongnao <huangtongnao@gmail.com>
2020-09-16 11:32:13 +0800

    sqrt 牛顿法

--- src/com/ssynhtn/easy/Sqrt.kt
+++ src/com/ssynhtn/easy/Sqrt.kt
@@ -4,43 +4,13 @@ import kotlin.math.sqrt
 
 class Sqrt {
     fun mySqrt(x: Int): Int {
-        if (x == 0 || x == 1) return x
-
-        var left = 2
-        var u = (1 shl 30)
-        while (u and x == 0) {
-            u = u shr 1
-            left++
+        var x1 = x
+        var x2 = x/2 + x%2
+        while(x2 < x1) {
+            x1 = x2
+            x2 = (x/x2 +x2)/2
         }
-
-        var right = (32 - left) / 2
-        var a = 1 shl right
-
-        var b = Math.min(1 shl (right + 1), 46340)
-        var m: Int
-        while (true) {
-            m = (a + b) / 2
-            var m2 = m * m
-            if (m2 > x) {
-                b = m
-                continue
-            }
-            if (m2 == x) {
-                return m
-            }
-
-            var m12 = m2 + m * 2 + 1
-            if (m12 == x) {
-                return (m + 1)
-            }
-            if (m12 > x || m12 < 0) {
-                return m
-            }
-
-            a = m + 1
-        }
-
-
+        return x1
     }
 }
 
