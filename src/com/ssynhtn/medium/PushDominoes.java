52424ddc0c7a11d4876a9bdc2850843245e4ac63
ssynhtn <ssynhtn@gmail.com>
2020-09-26 20:12:58 +0800

    update

--- src/com/ssynhtn/medium/PushDominoes.java
+++ src/com/ssynhtn/medium/PushDominoes.java
@@ -5,100 +5,50 @@ import java.util.*;
 public class PushDominoes {
     public String pushDominoes(String dominoes) {
         char[] chs = dominoes.toCharArray();
-        Queue<Integer> ls = new ArrayDeque<>();
-        Queue<Integer> rs = new ArrayDeque<>();
-
         int i = 0;
-        int len = dominoes.length();
-        while (i < len && chs[i] == '.') {
-            i++;
-        }
-        if (i == len) {
-            return dominoes;
-        }
+        int j = 0;
 
-
+        final int len = chs.length;
 
         while (true) {
-            // i at start of none .
-            if(i != 0) {
-                if (chs[i] == 'L') {
-                    ls.add(i);
-                } else {
-                    rs.add(i);
-                }
-            }
-
-            int j = i + 1;
-            while (j < len && chs[j] != '.') {
-                j++;
-            }
-
-            if (j == len) break;
-            // found .
-            if (j - i > 1 || i == 0) {
-                if (chs[j - 1] == 'L') {
-                    ls.add(j - 1);
-                } else {
-                    rs.add(j - 1);
-                }
-            }
-
-            i = j;
-            while (i < len && chs[i] == '.') {
+            while (i < len && chs[i] != '.') {
                 i++;
             }
 
             if (i == len) break;
-        }
 
-//        for (int i = 0; i < dominoes.length(); i++) {
-//            if (dominoes.charAt(i) == 'L') {
-//                ls.add(i);
-//            } else if (dominoes.charAt(i) == 'R') {
-//                rs.add(i);
-//            }
-//        }
-
-//        System.out.println("ls " + ls);
-//        System.out.println("rs " + rs);
-
-        while (!ls.isEmpty() || !rs.isEmpty()) {
-            int n = ls.size();
-            int m = rs.size();
-
-            for (i = 0; i < n; i++) {
-                int index = ls.remove();
-                if (index == 0) continue;
-                if (chs[index - 1] != '.') continue;
-                if (index == 1 || chs[index - 2] != 'R') {
-                    ls.add(index - 1);
-                    chs[index - 1] = 'L';
-
-
-                    if (index >= 3 && chs[index - 2] == '.' && chs[index - 3] == 'R') {
-                        chs[index - 2] = 'R';
-                        rs.add(index - 2);
-                    }
-                }
+            // i at start of .
+            j = i + 1;
+            while (j < len && chs[j] == '.') {
+                j++;
             }
 
-            for (i = 0; i < m; i++) {
-                int index = rs.remove();
-                if (index == dominoes.length() - 1) continue;
-                if (chs[index + 1] != '.') continue;
-                if (index == dominoes.length() - 2) {
-                    rs.add(index + 1);
-                    chs[index + 1] = 'R';
+            // j at end of .
+            if (i == 0 || chs[i - 1] == 'L') {
+                if (j < len && chs[j] == 'L') {
+                    // [i, j) to L
+                    while (i < j) {
+                        chs[i++] = 'L';
+                    }
+                }
+            } else {
+                if (j < len && chs[j] == 'L') {
+                    // [i, j) to middle
+                    int r = j - 1;
+                    while (i < r) {
+                        chs[i++] = 'R';
+                        chs[r--] = 'L';
+                    }
 
                 } else {
-                    if (chs[index + 2] != 'L') {
-                        rs.add(index + 1);
-                        chs[index + 1] = 'R';
+                    // to R
+                    while (i < j) {
+                        chs[i++] = 'R';
                     }
                 }
             }
 
+            i = j;
 
         }
 
@@ -106,6 +56,6 @@ public class PushDominoes {
     }
 
     public static void main(String[] args) {
-        System.out.println(new PushDominoes().pushDominoes(".L.R...LR..L.."));
+        System.out.println(new PushDominoes().pushDominoes("RR.L"));
     }
 }
