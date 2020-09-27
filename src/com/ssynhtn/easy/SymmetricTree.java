bf86807ddf02eeb95cd80db740f3019860919ce2
huangtongnao <huangtongnao@gmail.com>
2020-09-27 14:17:43 +0800

    symmetric tree

---
+++ src/com/ssynhtn/easy/SymmetricTree.java
@@ -0,0 +1,28 @@
+package com.ssynhtn.easy;
+
+public class SymmetricTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null) return right == null;
+        if (right == null) return false;
+        if (left.val != right.val) return false;
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+}
