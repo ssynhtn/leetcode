6d5f753832fa29a6e81ebcbc80968faf11965610
huangtongnao <huangtongnao@gmail.com>
2020-10-21 11:32:31 +0800

    save

--- src/com/ssynhtn/medium/SumofMutatedArrayClosesttoTarget.java
+++ src/com/ssynhtn/medium/SumofMutatedArrayClosesttoTarget.java
@@ -1,10 +1,88 @@
 package com.ssynhtn.medium;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class SumofMutatedArrayClosesttoTarget {
+    public int findBestValueBinary(int[] arr, int target) {
+        Arrays.sort(arr);
+        int n = arr.length;
+        int l = 0;
+        int r = arr[n - 1];
+
+        while (l < r) {
+            int m = (l + r) / 2;
+            int s = computeSum(arr, m);
+
+            if (s == target) return m;
+            if (s > target) {
+                r = m;
+            } else {
+                if (m > l) {
+                    l = m;
+                } else {
+                    int t = computeSum(arr, r);
+                    if (Math.abs(target - t) < Math.abs(target - s)) {
+                        return r;
+                    }
+                    return m;
+                }
+            }
+        }
+        return l;
+
+    }
+
+    private int computeSum(int[] arr, int m) {
+        int i = Arrays.binarySearch(arr, m);
+        if (i < 0) i = -1 - i;
+        int s = 0;
+        for (int j = 0; j < i; j++) {
+            s += arr[j];
+        }
+        s += m * (arr.length - i);
+        return s;
+    }
+
     public int findBestValue(int[] arr, int target) {
+        Arrays.sort(arr);
+        int n = arr.length;
+
+        int sum = 0;
+        int i = 0;
+
+        while (i < n && sum < target) {
+            sum += (arr[i] - (i > 0 ? arr[i-1] : 0)) * (n-i);
+            i++;
+        }
+
+        if (sum <= target) return arr[n-1];
+
+        int d = arr[i-1];
+        i--;
+
+//        System.out.println("if from " + i + " are " + arr[i] + "s, sum would be " + sum);
+        int diff = sum - target;
+
+        int c = i > 0 ? arr[i - 1] : 0;
+        int reduce = (d - c) * (n - i);
+//        System.out.println("d " + d + " c " + c + ", potential reduce " + reduce + ", diff " + diff);
+
+        if (reduce == diff) return c;
+        // reduce > diff
+        int u = diff / (n - i);
+//        System.out.println("" + (d-u) + " would lead to " + Math.abs(diff - u * (n - i)));
+//        System.out.println("" + (d-u-1) + " would lead to " + Math.abs(diff - (u + 1) * (n - i)));
+        if (Math.abs(diff - u * (n - i)) < Math.abs(diff - (u + 1) * (n - i))) {
+            return d - u;
+        } else {
+            return d - u - 1;
+        }
+
+
+    }
+
+
+    public int findBestValueOld(int[] arr, int target) {
         Arrays.sort(arr);
         int n = arr.length;
 
@@ -88,6 +166,6 @@ public class SumofMutatedArrayClosesttoTarget {
         int me = 1547 + 17423 * 4;
         int t = 1547 + 17422 * 4;
         System.out.println("me " + me + " t " + t + ", target " + 71237);
-        System.out.println(new SumofMutatedArrayClosesttoTarget().findBestValue(new int[]{1547,83230,57084,93444,70879}, 71237));
+        System.out.println(new SumofMutatedArrayClosesttoTarget().findBestValueBinary(new int[]{1547,83230,57084,93444,70879}, 71237));
     }
 }
