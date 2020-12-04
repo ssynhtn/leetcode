d21f1b0c9095781110898662cce838365871c11a
huangtongnao <huangtongnao@gmail.com>
2020-12-04 16:04:46 +0800

    save

--- src/com/ssynhtn/hard/MaxConcatDigitsMerge.java
+++ src/com/ssynhtn/hard/MaxConcatDigitsMerge.java
@@ -4,87 +4,38 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MaxConcatDigits {
-    Map<Long, int[]> cache = new HashMap<>();
-    private int[] findMaxNum(int[] nums1, int[] nums2, int[][] nextIndex1, int[][] nextIndex2, int l1, int r1, int l2, int r2, int k) {
-        long kLong = k;
-        long l1Long = l1;
-        long l2Long = l2;
-        long key = (kLong << 40) | (l1Long << 20) | l2Long;
-        int[] c = cache.get(key);
-        if (c != null) return c;
-        int[] out = new int[k];
+public class MaxConcatDigitsMerge {
+    private void findMaxNum(int[] nums, int l1, int r1, int k, int[] out) {
+        if (k == 0) return;
         int index = 0;
-        while (k > 0) {
-            int d1 = -1, next1 = -1;
-
-            if (r1 > l1) {
-                for (d1 = 9; d1 >= 0; d1--) {
-                    next1 = nextIndex1[l1][d1];
-
-                    if (next1 >= l1 && next1 < r1 && next1 <= r1 + r2 - l2 - k) {
-                        break;
-                    }
-                }
+        int remain = r1 - l1;
+        for (int i = l1; i < r1; i++) {
+            while (index > 0 && nums[i] > out[index - 1] && remain > k) {
+                index--;
+                remain--;
             }
 
-            int d2 = -1, next2 = -1;
-            if (r2 > l2) {
-                for (d2 = 9; d2 >= 0; d2--) {
-                    next2 = nextIndex2[l2][d2];
-
-                    if (next2 >= l2 && next2 < r2 && next2 <= r2 + r1 - l1 - k) {
-                        break;
-                    }
-                }
+            if (index == k) {
+                remain--;
+                continue;
             }
-
-            if (d1 > d2) {
-                out[index++] = d1;
-                k--;
-                l1 = next1 + 1;
-            } else if (d2 > d1) {
-                out[index++] = d2;
-                k--;
-                l2 = next2 + 1;
-            } else {
-                // d1 == d2
-                out[index++] = d1;
-                k--;
-                // 使用next1
-//                System.out.println("same d " + d1 + " at " + next1 + " and " + next2);
-                int[] first = findMaxNum(nums1, nums2, nextIndex1, nextIndex2,  next1 + 1, r1, l2, r2, k);
-//                System.out.println("rec for first arr is ");
-//                for (int j = index; j < index + k; j++) {
-//                    System.out.print(out[j] + ",");
-//                }
-//                System.out.println();
-                int[] second = findMaxNum(nums1, nums2, nextIndex1, nextIndex2, l1, r1, next2 + 1, r2, k);
-//                System.out.println("rec for second arr is ");
-//                for (int j = 0; j < k; j++) {
-//                    System.out.print(buff[j] + ",");
-//                }
-//                System.out.println();
-
-                int end = k;
-                int i = 0;
-                while (i < end && first[i] == second[i]) {
-                    i++;
-                }
-
-                if (i < end && first[i] < second[i]) {
-                    System.arraycopy(second, 0, out, index, k);
-                } else {
-                    System.arraycopy(first, 0, out, index, k);
-                }
-
-                break;
-            }
-
+            out[index++] = nums[i];
         }
-
-        cache.put(key, out);
-        return out;
+//        while (k > 0) {
+//            int d1 = -1, next1 = -1;
+//
+//            for (d1 = 9; d1 >= 0; d1--) {
+//                next1 = nextIndex1[l1][d1];
+//
+//                if (next1 >= l1 && next1 <= r1 - k) {
+//                    break;
+//                }
+//            }
+//
+//            out[index++] = d1;
+//            k--;
+//            l1 = next1 + 1;
+//        }
 
     }
 
@@ -94,11 +45,42 @@ public class MaxConcatDigits {
         int n = nums1.length;
         int m = nums2.length;
 
-        int[][] nextIndex1 = buildNextIndex(nums1, n);
-        int[][] nextIndex2 = buildNextIndex(nums2, m);
 
-        return findMaxNum(nums1, nums2, nextIndex1, nextIndex2, 0, n, 0, m, k);
+        int min = Math.max(0, k - m);
+        int max = Math.min(n, k);
 
+        int[] res = null;
+        int[] out = new int[k];
+        int[] buff1 = new int[k];
+        int[] buff2 = new int[k];
+        for (int k1 = min; k1 <= max; k1++) {
+            findMaxNum(nums1, 0, n, k1, buff1);
+            findMaxNum(nums2, 0, m, k - k1, buff2);
+            merge(buff1, buff2, out, 0, 0, k1, 0, k - k1);
+
+            if (res == null) {
+                res = out;
+                out = new int[k];
+            } else if (larger(out, res)) {
+                int[] temp = res;
+                res = out;
+                out = temp;
+            }
+
+        }
+        return res;
+
+    }
+
+    // out > res
+    private boolean larger(int[] out, int[] res) {
+        int i = 0;
+        int n = out.length;
+        while (i < n && out[i] == res[i]) {
+            i++;
+        }
+
+        return i < n && out[i] > res[i];
     }
 
     private int[][] buildNextIndex(int[] nums, int n) {
@@ -112,22 +94,54 @@ public class MaxConcatDigits {
         return next;
     }
 
+    private void merge(int[] nums1, int[] nums2, int[] out, int index, int i, int r1, int j, int r2) {
+        while (i < r1 && j < r2) {
+            int i0 = i;
+            int j0 = j;
+            while (i0 < r1 && j0 < r2 && nums1[i0] == nums2[j0]) {
+                i0++;
+                j0++;
+            }
+
+            if (i0 == r1) {
+                out[index++] = nums2[j++];
+            } else if (j0 == r2) {
+                out[index++] = nums1[i++];
+            } else if (nums1[i0] > nums2[j0]) {
+                out[index++] = nums1[i++];
+            } else {
+                out[index++] = nums2[j++];
+            }
+        }
+
+        while (i < r1) {
+            out[index++] = nums1[i++];
+        }
+        while (j < r2) {
+            out[index++] = nums2[j++];
+        }
+    }
+
+
     public static void main(String[] args) {
-//        int k = 5;
-//        int[] ones = new int[]{
-//                2,4,
-//        };
-//        int[] twos = new int[]{
-//                2,5,2,
-//        };
-//
-        int k = 80;
+        int k = 2;
         int[] ones = new int[]{
-                1,5,8,1,4,0,8,5,0,7,0,5,7,6,0,5,5,2,4,3,6,4,6,6,3,8,1,1,3,1,3,5,4,3,9,5,0,3,8,1,4,9,8,8,3,4,6,2,5,4,1,1,4,6,5,2,3,6,3,5,4,3,0,7,2,5,1,5,3,3,8,2,2,7,6,7,5,9,1,2
+
         };
         int[] twos = new int[]{
-                7,8,5,8,0,1,1,6,1,7,6,9,6,6,0,8,5,8,6,3,4,0,4,6,7,8,7,7,7,5,7,2,5,2,1,9,5,9,3,7,3,9,9,3,1,4,3,3,9,7,1,4,4,1,4,0,2,3,1,3,2,0,2,4,0,9,2,0,1,3,9,1,2,2,6,6,9,3,6,0
+                5,5,4,7
         };
+        int[] buff = new int[2];
+        new MaxConcatDigitsMerge().findMaxNum(twos, 0, 4, 2, buff);
+        System.out.println(Arrays.toString(buff));
+//
+//        int k = 80;
+//        int[] ones = new int[]{
+//                1,5,8,1,4,0,8,5,0,7,0,5,7,6,0,5,5,2,4,3,6,4,6,6,3,8,1,1,3,1,3,5,4,3,9,5,0,3,8,1,4,9,8,8,3,4,6,2,5,4,1,1,4,6,5,2,3,6,3,5,4,3,0,7,2,5,1,5,3,3,8,2,2,7,6,7,5,9,1,2
+//        };
+//        int[] twos = new int[]{
+//                7,8,5,8,0,1,1,6,1,7,6,9,6,6,0,8,5,8,6,3,4,0,4,6,7,8,7,7,7,5,7,2,5,2,1,9,5,9,3,7,3,9,9,3,1,4,3,3,9,7,1,4,4,1,4,0,2,3,1,3,2,0,2,4,0,9,2,0,1,3,9,1,2,2,6,6,9,3,6,0
+//        };
 
 //        int[] ones = new int[]{
 //                8,9,7,3,5,9,1,0,8,5,3,0,9,2,7,4,8,9,8,1,0,2,0,2,7,2,3,5,4,7,4,1,4,0,1,4,2,1,3,1,5,3,9,3,9,0,1,7,0,6,1,8,5,6,6,5,0,4,7,2,9,2,2,7,6,2,9,2,3,5,7,4,7,0,1,8,3,6,6,3,0,8,5,3,0,3,7,3,0,9,8,5,1,9,5,0,7,9,6,8,5,1,9,6,5,8,2,3,7,1,0,1,4,3,4,4,2,4,0,8,4,6,5,5,7,6,9,0,8,4,6,1,6,7,2,0,1,1,8,2,6,4,0,5,5,2,6,1,6,4,7,1,7,2,2,9,8,9,1,0,5,5,9,7,7,8,8,3,3,8,9,3,7,5,3,6,1,0,1,0,9,3,7,8,4,0,3,5,8,1,0,5,7,2,8,4,9,5,6,8,1,1,8,7,3,2,3,4,8,7,9,9,7,8,5,2,2,7,1,9,1,5,5,1,3,5,9,0,5,2,9,4,2,8,7,3,9,4,7,4,8,7,5,0,9,9,7,9,3,8,0,9,5,3,0,0,3,0,4,9,0,9,1,6,0,2,0,5,2,2,6,0,0,9,6,3,4,1,2,0,8,3,6,6,9,0,2,1,6,9,2,4,9,0,8,3,9,0,5,4,5,4,6,1,2,5,2,2,1,7,3,8,1,1,6,8,8,1,8,5,6,1,3,0,1,3,5,6,5,0,6,4,2,8,6,0,3,7,9,5,5,9,8,0,4,8,6,0,8,6,6,1,6,2,7,1,0,2,2,4,0,0,0,4,6,5,5,4,0,1,5,8,3,2,0,9,7,6,2,6,9,9,9,7,1,4,6,2,8,2,5,3,4,5,2,4,4,4,7,2,2,5,3,2,8,2,2,4,9,8,0,9,8,7,6,2,6,7,5,4,7,5,1,0,5,7,8,7,7,8,9,7,0,3,7,7,4,7,2,0,4,1,1,9,1,7,5,0,5,6,6,1,0,6,9,4,2,8,0,5,1,9,8,4,0,3,1,2,4,2,1,8,9,5,9,6,5,3,1,8,9,0,9,8,3,0,9,4,1,1,6,0,5,9,0,8,3,7,8,5
@@ -139,7 +153,7 @@ public class MaxConcatDigits {
 
 //        System.out.println("ones len " + ones.length + ", twos len " + twos.length);
 
-        int[] res = new MaxConcatDigits().maxNumber(ones, twos, k);
+        int[] res = new MaxConcatDigitsMerge().maxNumber(ones, twos, k);
         for (int x : res) {
             System.out.print(x + ",");
         }
