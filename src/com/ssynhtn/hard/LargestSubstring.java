6386ac342d76d357f5039316e4149f8e202756dd
huangtongnao <huangtongnao@gmail.com>
2020-12-18 09:15:48 +0800

    save

--- src/com/ssynhtn/hard/LargestSubstring.java
+++ src/com/ssynhtn/hard/LargestSubstring.java
@@ -10,6 +10,32 @@ class LargestSubstring {
     public String lastSubstring(String s) {
         char[] chs = s.toCharArray();
         int n = chs.length;
+
+        int i = n-1;
+
+        // [i, ~) current largest
+        for (int j = n - 2; j >= 0; j--) {
+            if (chs[j] < chs[i]) continue;
+
+            // jth >= ith
+            int l = j;
+            int r = i;
+            while (l < i && r < n && chs[l] == chs[r]) {
+                l++;
+                r++;
+            }
+
+            if (r == n || l == i || chs[l] > chs[r]) {
+                i = j;
+            }
+        }
+
+        return new String(chs, i, n - i);
+
+    }
+    public String lastSubstringZ(String s) {
+        char[] chs = s.toCharArray();
+        int n = chs.length;
         List<Integer>[] indexes = new List[26];
 
         for (int i = 0; i < 26; i++) {
@@ -42,19 +68,21 @@ class LargestSubstring {
 
     // i < j
     private boolean larger(char[] chs, int i, int j) {
+        int j0 = j;
         if (j == i + 1) return true;
         int n = chs.length;
-        while (i < n && j < n && chs[i] == chs[j]) {
+        while (i < j0 && j < n && chs[i] == chs[j]) {
             i++;
             j++;
         }
 
         if (j == n) return true;
+        if (i == j0) return true;
         return chs[i] > chs[j];
     }
 
     public static void main(String[] args) {
-        char[] chs = new char[100000];
+        char[] chs = new char[10];
         Arrays.fill(chs, 'a');
         System.out.println(new LargestSubstring().lastSubstring(new String(chs)));
     }
